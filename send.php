aece612d9fd0febf295561a02bb83bbe0d4cc02b
Irfan Syah <106759624+Irfan123fw@users.noreply.github.com>
2022-11-13 23:31:59 +0700

    Update send.php

--- send.php
+++ send.php
@@ -4,7 +4,7 @@
       $email = $_POST['email'];
       $pesan = $_POST['pesan'];
       $no_wa = $_POST['noWa'];
-      header("location:https://api.whatsapp.com/send?phone=$no_wa&text=Nama:$name
+      header("location:https://api.whatsapp.com/send?phone=6282172716552&text=Nama:$name
       Email:$email Pesan:$pesan");
 
     }else {
